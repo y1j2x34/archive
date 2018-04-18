d7b92279effc302d4e0bef080ba08a3299aa0ee8
y1j2x34 <y1j2x34@qq.com>
2018-04-18 23:38:41 +0800

    chore: environment configurations

--- build/clean.js
+++ build/clean.js
@@ -1,9 +1,12 @@
 var fs = require('fs');
 var glob = require('glob');
 
-glob('dist/**/*.*', function(err, matches) {
-    matches.forEach(function(filepath) {
-        fs.unlinkSync(filepath);
+if(fs.existsSync('dist/')) {
+    glob('dist/**/*.*', function(err, matches) {
+        matches.forEach(function(filepath) {
+            fs.unlinkSync(filepath);
+        });
+        fs.rmdirSync('dist/');
     });
-    fs.rmdirSync('dist/');
-});
+}
+
